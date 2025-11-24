68bd4fb879d9ee524e7de8ae738ef4c53f382d17
aoirint <aoirint@gmail.com>
2025-11-24 16:28:32 +0900

    feat: CollectAll, CollectTool command placeholder

--- CollectCruiserItemCompany/Utils/KeywordUtils.cs
+++ CollectCruiserItemCompany/Utils/KeywordUtils.cs
@@ -33,4 +33,32 @@ internal static class KeywordUtils
 
         return false;
     }
+
+    internal static bool IsToolArgument(string keyword)
+    {
+        keyword = keyword.ToLower();
+
+        var firstCharacter = keyword.Length > 0 ? keyword[..1] : "";
+
+        if (firstCharacter == "t")
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    internal static bool IsAllArgument(string keyword)
+    {
+        keyword = keyword.ToLower();
+
+        var firstCharacter = keyword.Length > 0 ? keyword[..1] : "";
+
+        if (firstCharacter == "a")
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
