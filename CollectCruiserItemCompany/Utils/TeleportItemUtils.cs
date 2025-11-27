f2ccf5df35ba09920686d6686aaf33ca89c0a9a7
aoirint <aoirint@gmail.com>
2025-11-27 22:19:29 +0900

    fix: import

--- CollectCruiserItemCompany/Utils/TeleportItemUtils.cs
+++ CollectCruiserItemCompany/Utils/TeleportItemUtils.cs
@@ -3,6 +3,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using BepInEx.Logging;
+using CollectCruiserItemCompany.Extensions;
 using GameNetcodeStuff;
 using HarmonyLib;
 using Unity.Netcode;
